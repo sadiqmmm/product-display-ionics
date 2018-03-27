87adcd39aa4801f06caa82d2596cd1262976839e
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-03-27 16:58:56 +0530

    Fix the tab best seller

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductProvider } from '../../providers/product/product';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public bestSellerProducts = [];
+  constructor(public navCtrl: NavController, private productProvide: ProductProvider) {
 
-  constructor(public navCtrl: NavController) {
-
+  } 
+  ionViewDidLoad() {
+    this.productProvide.getProducts()
+    .subscribe((allProduct) => {
+      this.bestSellerProducts = allProduct.filter((product => product.bestSeller == true));
+    });
   }
 
 }
