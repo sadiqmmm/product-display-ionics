36e60e9d4e5b8edb46c83aa705b51188398d7b7c
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-03-27 14:23:06 +0530

    add the data.json and product images

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
+  }
+  ionViewDidLoad() {
+    this.http.get<any[]>('/assets/data.json') 
+    .subscribe(data => console.log(data));
   }
 
 }
